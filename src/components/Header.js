49245df52eb25f5567bcc3f733a63b8f7d7c30f2
terryloughran <terryloughran@gmail.com>
2024-03-04 15:29:51 +0000

    Header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
+import Logo from '../assets/icontask.jpeg';
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className="logo">
+        <img src={Logo} alt="taskmate logo"></img>
+        <span>TaskMate</span>
+      </div>
+      <div className="themeSelector">
+        <span className="light"></span>
+        <span className="medium"></span>
+        <span className="dark"></span>
+        <span className="gOne"></span>
+        <span className="gTwo"></span>
+        <span className="gThree"></span>
+      </div>
+    </header>
   );
 };
