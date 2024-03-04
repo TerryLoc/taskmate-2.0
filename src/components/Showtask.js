01a7c0d5790c44aa49eb7fc5b61b3c42fb22369b
terryloughran <terryloughran@gmail.com>
2024-03-04 16:17:09 +0000

    showtask display

--- src/components/Showtask.js
+++ src/components/Showtask.js
@@ -1,7 +1,32 @@
 export const Showtask = () => {
+  const tasks = [
+    { id: 1001, name: 'TASK A', time: '1:56:56 PM - 10/2/2024' },
+    { id: 1002, name: 'TASK B', time: '1:56:56 PM - 10/2/2024' },
+    { id: 1003, name: 'TASK C', time: '1:56:56 PM - 10/2/2024' },
+    { id: 1004, name: 'TASK D', time: '1:56:56 PM - 10/2/2024' },
+  ];
+
   return (
-    <div>
-      <h1>Showtask</h1>
-    </div>
+    <section className="showTask">
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">0</span>
+        </div>
+        <button className="clearAll">Clear All</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li>
+            <p>
+              <span className="name">{task.name}</span>
+              <span className="time">{task.time}</span>
+            </p>
+            <i className="bi bi-pencil"></i>
+            <i className="bi bi-trash3"></i>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
